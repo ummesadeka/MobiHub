2fd97f1396c96e0222e56db8ffa8a4a555fe012f
ummesadeka <sadekaahmed53@gmail.com>
2021-06-08 20:25:07 +0600

    updated

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCrlo-xUP5_Intugwbp_QTm6IUVl7mXZ7Q",
+    authDomain: "mobihub-sami.firebaseapp.com",
+    projectId: "mobihub-sami",
+    storageBucket: "mobihub-sami.appspot.com",
+    messagingSenderId: "494165893331",
+    appId: "1:494165893331:web:df02c3dd4952cafd1b7426"
+  };
+   export default firebaseConfig;
